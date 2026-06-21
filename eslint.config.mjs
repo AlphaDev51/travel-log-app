c042f1f27174c6cde0eaf73eba326deecc463c19
Alpha Barry <alphaxbarry99@gmail.com>
2026-06-21 14:16:40 +0000

    :sparkles: add lint workflow fix1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default withNuxt(
         semi: true,
         quotes: "double",
       },
+      ignores: ["pnpm-store/**"],
     },
     {
       rules: {
@@ -29,6 +30,7 @@ export default withNuxt(
           //   tsconfigRootdDir: ".",
           // },
         ],
+
         "unicorn/filename-case": [
           "error",
           {
