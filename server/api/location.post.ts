116d2f11785bbe0623f002b4cdfaa68ecd548a20
Alpha Barry <alphaxbarry99@gmail.com>
2026-07-14 17:15:03 +0000

    :sparkles: Location Names / Slugs Are URL Friendly and Validated to Be Unique

--- server/api/location.post.ts
+++ server/api/location.post.ts
@@ -1,5 +1,10 @@
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
 import db from "~~/src/db";
 import { InsertLocation, location } from "~~/src/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 10);
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -12,7 +17,6 @@ export default defineEventHandler(async (event) => {
   const result = await readValidatedBody(event, InsertLocation.safeParse);
 
   if (!result.success) {
-    //  ` ${i.path.join("")}: ${i.message}`
     const data = result
       .error
       ?.issues
@@ -34,11 +38,56 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [created] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
-    userId: event.context.user.id,
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where:
+      and(
+        eq(location.name, result.data.name),
+        eq(location.userId, event.context.user.id),
+      ),
+  });
 
-  return created;
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "A location with that name already exist",
+    }));
+  }
+
+  let slug = slugify(result.data.name);
+  let existingSlug = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+
+  while (existingSlug) {
+    const randomID = nanoid();
+    const newSlug = `${slug}-${randomID}`;
+
+    existingSlug = !!(await db.query.location.findFirst({
+      where: eq(location.slug, newSlug),
+    }));
+
+    if (!existingSlug) {
+      slug = newSlug;
+    }
+  }
+  try {
+    const [created] = await db.insert(location).values({
+      ...result.data,
+      slug,
+      userId: event.context.user.id,
+    }).returning();
+
+    return created;
+  }
+  catch (e: any) {
+    const error = e;
+
+    if (error.cause?.message?.includes("SQLite error: UNIQUE constraint failed: location.slug")) {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "the slug must be unique (the name is used to generate the slug)",
+      }));
+    }
+    throw error;
+  }
 });
